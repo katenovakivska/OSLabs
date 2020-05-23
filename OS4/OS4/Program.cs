d07cf94ea1b5252a233a416bcd20445f6b4b425b
Kate Novakivska <kate.nova14@gmail.com>
2020-05-23 14:25:38 +0300

    add task 5

---
+++ OS4/OS4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+
+            var a = new int[100, 100];
+
+            for (int i = 99; i > 0; i--)
+            {
+                for (int j = 99; j > 0; j--)
+                {
+                    a[j, i] = a[j - 1, i - 1] + 1;
+                    
+                }
+               
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    a[j, i]--;
+                }
+            }
+            var finish = DateTime.Now;
+
+            Console.WriteLine($"Time of work: {finish - start}");
+            Console.ReadKey();
+        }
+    }
+}
